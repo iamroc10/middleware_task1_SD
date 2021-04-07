b0131d26c33083e7f6b1d5c22de56c47f2404d70
iamroc10 <79842817+iamroc10@users.noreply.github.com>
2021-04-07 22:07:15 +0200

    Update client_xmlrpc.py

--- client_xmlrpc.py
+++ client_xmlrpc.py
@@ -3,53 +3,54 @@ import time
 
 proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
 
-#creació de 3 WORKERS
+
+def switch(num):
+    result = 0
+    if (num == 1): result = proxy.createWorker()
+    if (num == 2): result = proxy.listWorker()
+    if (num == 3): 
+        quin = int(input("ID worker a eliminar (l'id comença per 1):"))
+        result = proxy.deleteWorker(quin)
+    if ((num == 4) or (num == 5)):
+        fitxers = int(input("Quants fitxers a tractar? "))
+        i = 0
+        fitxer = ''
+        while (i < fitxers):
+            nom_fitxer = input("Nom fitxer: ")
+            if ((fitxers > 1) and (i < fitxers-1)):
+                fitxer = str(fitxer) + str(nom_fitxer) + '*'
+            if (i == fitxers-1):
+                fitxer = str(fitxer) + str(nom_fitxer)
+            i = i + 1
+        if (num == 4):
+            task = 'wordCount'
+        else: task = 'countingWords'
+
+        job_id = proxy.tractamentCua(task, fitxer)
+        time.sleep(5)
+        print('\nRESULTAT '+ task + ': ')
+        result = proxy.getResult(str(job_id), task)
+
+    if (num == 6): result = -1
+    
+    return result
+
+
+def inicialitzacio():
+    print('\n1. Create Worker')
+    print('2. List Workers')
+    print('3. Delete Worker')
+    print('4. Word Count')
+    print('5. Counting Words')
+    print('6. EXIT\n')
+
+
+
+cond = 7
+print('CREACIÓ WORKER AUTOMÀTIC')
 print(proxy.createWorker())
-print(proxy.createWorker())
-print(proxy.createWorker())
-
-#LIST WORKER
-print(proxy.listWorker())
-
-#DELETE WORKER ( 1 )
-print(proxy.deleteWorker(1))
-
-#LIST WORKER
-print(proxy.listWorker())
-
-#WORDCOUNT amb 1 fitxer
-job_id = proxy.tractamentCua('wordCount', 'fitxerText.txt')
-time.sleep(2)
-print('\nResultat WORDCOUNT amb 1 fitxer:')
-print(proxy.getResult(str(job_id), 'wordCount'))
-
-#WORDCOUNT amb 1 fitxer
-job_id = proxy.tractamentCua('wordCount', 'fitxerText2.txt')
-time.sleep(2)
-print('\nResultat WORDCOUNT amb 1 fitxer:')
-print(proxy.getResult(str(job_id), 'wordCount'))
-
-#COUNTINGWORDS  amb 1 fitxer
-job_id = proxy.tractamentCua('countingWords', 'fitxerText.txt')
-time.sleep(2)
-print('\nResultat COUNTINGWORDS amb 1 fitxer:')
-print(proxy.getResult(str(job_id), 'countingWords'))
-
-#COUNTINGWORDS amb 1 fitxer
-job_id = proxy.tractamentCua('countingWords', 'fitxerText2.txt')
-time.sleep(2)
-print('\nResultat COUNTINGWORDS amb 1 fitxer:')
-print(proxy.getResult(str(job_id), 'countingWords'))
-
-#COUNTINGWORDS amb 2 fitxers
-job_id = proxy.tractamentCua('countingWords', 'fitxerText.txt*fitxerText2.txt')
-time.sleep(5)
-print('\nResultat COUNTINGWORDS amb 2 fitxers:')
-print(proxy.getResult(str(job_id), 'countingWords'))
-
-#WORDCOUNT amb 3 fitxers
-job_id = proxy.tractamentCua('wordCount', 'fitxerText.txt*fitxerText2.txt*fitxerText.txt')
-time.sleep(5)
-print('\nResultat WORDCOUNT amb 3 fitxers:')
-print(proxy.getResult(str(job_id), 'wordCount'))
-
+while (cond != -1):
+    inicialitzacio()
+    case = int(input("Escull una opció: "))
+    cond = switch(case)
+    print(cond)
